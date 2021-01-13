ab3d12d68d2eddb8e65f08200a64e0e5f92ec402
Iris SF <iris.kosy@gmail.com>
2021-01-13 13:45:26 +0100

    Agregar archivos de proyecto.

---
+++ DI_Tema5_Ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Realiza un servidor de fecha y hora. Aceptará los comandos: HORA (hora, minutos y segundos)),
+FECHA (día, mes y año), TODO (hora y fecha), APAGAR (El servidor se cierra). Dependiendo del
+comando que reciba enviará la información correspondiente.
+Por la brevedad de los mensajes transmitidos, no es necesario que sea mutihilo. Y por tanto para
+poder tramitar varios clientes en cuanto se envíe el dato pedido se cerrará la conexión.
+Haz un cliente simple en modo gráfico con cuatro botones (uno por comando) y un TextBox o Label
+para mostrar el resultado y probarlo. También tendrá la posibilidad de indicarle la IP y puerto de
+conexión (aunque venga con unos predefinidos) en un formulario de diálogo. */
+
+namespace DI_Tema5_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
